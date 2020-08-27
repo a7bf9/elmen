6fb6bf101f7f95a58372f5be53c4a18ffb430f97
lloydmeins <github@lloydmeins.de>
2020-08-27 22:03:37 +0200

    Add detail.html template

--- elmen/polls/views.py
+++ elmen/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 
@@ -6,14 +6,17 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("Youre looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question': question,
+    }
+    return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
     response = "Youre looking at the results of question %s."
