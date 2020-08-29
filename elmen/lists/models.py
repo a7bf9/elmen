7f38a01fa4c2c720c3d8f0497cd1057f3d751ab7
lloydmeins <github@lloydmeins.de>
2020-08-29 21:27:30 +0200

    Add models List, Entry and Link

--- elmen/lists/models.py
+++ elmen/lists/models.py
@@ -4,5 +4,13 @@ class List(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
 
+class Link(models.Model):
+    address = models.CharField(max_length=100)
+
 class Entry(models.Model):
     list = models.ForeignKey(List, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    short_description = models.CharField(max_length=300)
+    long_description = models.TextField()
+    active = models.BooleanField(default=True)
+    web_address = models.ForeignKey(Link, on_delete=models.CASCADE)
